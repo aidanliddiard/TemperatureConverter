9f028b7486811892d40659940353d6d4e71f9ea6
Aidan Liddiard <Aidan.Liddiard@cvent.com>
2024-05-02 14:04:06 -0700

    ooops forgot to commit earlier

---
+++ convert-f-to-c.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Convert Fahrenheit to Celsius</h2>
+
+            <input type="number" name="fahrenheit"/>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">Convert</button>
+
+
+</body>
+</html> 
